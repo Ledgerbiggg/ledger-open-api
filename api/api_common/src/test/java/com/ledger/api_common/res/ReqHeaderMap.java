79e2324ee52136d47ac3d2f4bf49129d2cd2fac3
1·231 <123213>
2023-10-10 17:46:21 +0800

    Changes

---
+++ api/api_common/src/test/java/com/ledger/api_common/res/ReqHeaderMap.java
@@ -0,0 +1,13 @@
+package com.ledger.api_common.res;
+
+import org.springframework.http.HttpHeaders;
+
+import java.util.HashMap;
+
+public class ReqHeaderMap extends HashMap<HttpHeaders,String> {
+
+
+
+
+
+}
